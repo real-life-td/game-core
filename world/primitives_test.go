30e7a953cd4d4781295e695860911fdd4c5683cc
Joseph Naberhaus <naberhausj@gmail.com>
2020-04-13 17:53:44 -0500

    feat(primitives_test.go): broke tests

--- world/primitives_test.go
+++ world/primitives_test.go
@@ -9,6 +9,6 @@ func TestNewNode(t *testing.T) {
 	n := NewNode(1, 2, 3)
 
 	require.Equal(t, uint64(1), n.Id())
-	require.Equal(t, 2, n.X())
+	require.Equal(t, 44, n.X())
 	require.Equal(t, 3, n.Y())
 }
