ef8ec3db996de49a735dafe19a9bdd385c168821
Joseph Naberhaus <naberhausj@gmail.com>
2020-04-15 23:55:45 -0500

    feat(road.go): add cost to roads
    This is not a breaking change since nothing depends on roads yet

--- world/road.go
+++ world/road.go
@@ -3,13 +3,15 @@ package world
 type Road struct {
 	id           uint64
 	node1, node2 *Node
+	cost int
 }
 
-func NewRoad(id uint64, node1, node2 *Node) *Road {
+func NewRoad(id uint64, node1, node2 *Node, cost int) *Road {
 	road := new(Road)
 	road.id = id
 	road.node1 = node1
 	road.node2 = node2
+	road.cost = cost
 	return road
 }
 
@@ -23,4 +25,8 @@ func (r *Road) Node1() *Node {
 
 func (r *Road) Node2() *Node {
 	return r.node2
+}
+
+func (r *Road) Cost() int {
+	return r.cost
 }
