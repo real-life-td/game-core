0664fd2119b0dc53817b54bc3d83ab3c084195cb
Joseph Naberhaus <naberhausj@gmail.com>
2020-04-23 11:05:03 -0500

    feat(ids.go): change id to use 16/48 split
    This will allow ids to be more easiely consumed by JavaScript if that ever occurs

--- world/ids.go
+++ world/ids.go
@@ -2,6 +2,14 @@ package world
 
 import "errors"
 
+// Id contains 16 bits of type information followed by 48 bits of a base id. This approach has 2 benefits
+// 1. Simplifies the process of creating unique ids for objects
+//		a. For objects with ids from overpass we can just re-use those
+//		b. For other objects we can just use a simple counter
+// 2. Compactly adds type information to each object
+//
+// The reason for the 16/48 split is that if we ever want to consume these components in JavaScript it will allow us
+// to safely fit the base id into a 64 bit floating point number (max safe int being 53 bits long)
 type Id uint64
 type Type uint8
 
@@ -17,23 +25,23 @@ func typeValid(t Type) bool {
 	return t < typeEnd
 }
 
-// Adds type information. This requires the 8 most significant bits of the basedId to be empty or error will be thrown
+// Adds type information. This requires the 16 most significant bits of the basedId to be empty or error will be thrown
 func BaseIdToId(baseId uint64, t Type) (id Id, err error) {
-	if baseId >> 56 != 0 {
-		return 0, errors.New("8 most significant bits of base id must be 0")
+	if baseId >> 48 != 0 {
+		return 0, errors.New("16 most significant bits of base id must be 0")
 	}
 
 	if !typeValid(t) {
 		return 0, errors.New("invalid type")
 	}
 
-	return Id((uint64(t) << 56) | baseId), nil
+	return Id((uint64(t) << 48) | baseId), nil
 }
 
 func IdType(id Id) Type {
-	return Type(id >> 56)
+	return Type(id >> 48)
 }
 
 func BaseId(id Id) uint64 {
-	return uint64(id) << 8 >> 8
+	return uint64(id) & 0x0000FFFFFFFFFFFF
 }
--- world/ids_test.go
+++ world/ids_test.go
@@ -13,27 +13,27 @@ func TestTypeIota(t *testing.T) {
 }
 
 func TestBaseIdToId(t *testing.T) {
-	_, err := BaseIdToId(0x01FFFFFFFFFFFFFF, RoadType)
-	require.Error(t, err)
+	_, err := BaseIdToId(0x0001FFFFFFFFFFFF, RoadType)
+	require.Error(t, err, "base id cannot be more than 48 bits")
 
 	_, err = BaseIdToId(0, Type(math.MaxUint8))
-	require.Error(t, err)
+	require.Error(t, err, "type must be one of supplied constants")
 
 	id, err := BaseIdToId(1, NodeType)
 	require.NoError(t, err)
-	require.Equal(t, Id(0x0100000000000001), id)
+	require.Equal(t, Id(0x0001000000000001), id)
 
-	id, err = BaseIdToId(0x00FFFFFFFFFFFFFF, RoadType)
+	id, err = BaseIdToId(0x0000FFFFFFFFFFFF, RoadType)
 	require.NoError(t, err)
-	require.Equal(t, Id(0x00FFFFFFFFFFFFFF), id)
+	require.Equal(t, Id(0x0000FFFFFFFFFFFF), id)
 }
 
 func TestBaseId(t *testing.T) {
 	require.Equal(t, uint64(1), BaseId(Id(0x0000000000000001)))
-	require.Equal(t, uint64(4), BaseId(Id(0x0100000000000004)))
+	require.Equal(t, uint64(4), BaseId(Id(0x0001000000000004)))
 }
 
 func TestIdType(t *testing.T) {
 	require.Equal(t, Type(0), IdType(Id(0x0000000000000001)))
-	require.Equal(t, Type(1), IdType(Id(0x0100000000000004)))
+	require.Equal(t, Type(1), IdType(Id(0x0001000000000004)))
 }
