f874ec5d791caa89d18f2042d3aa8d61dc54e540
Joseph Naberhaus <naberhausj@gmail.com>
2020-04-30 13:25:39 -0500

    feat(building.go): add road connections to building model

--- world/building_test.go
+++ world/building_test.go
@@ -9,7 +9,15 @@ import (
 func TestNewBuilding(t *testing.T) {
 	expectedPoints := []*Node{NewNode(1, 2, 3), NewNode(4, 5, 6)}
 	b := NewBuilding(0, expectedPoints)
+
 	require.Equal(t, b.Id(), Id(0))
 	require.Equal(t, expectedPoints, b.points)
 	require.Equal(t, primitives.NewRectangle(2, 3, 5, 6), b.Bounds())
+}
+
+func TestBuilding_Connections(t *testing.T) {
+	b := NewBuilding(0, nil)
+	b.connections = []*Road{NewRoad(1, nil)}
+
+	require.Equal(t, b.Connections(), []*Road{NewRoad(1, nil)})
 }
