9b4e54b83359622db25c1ea80c9c728878647d71
Joseph Naberhaus <naberhausj@gmail.com>
2020-06-18 21:15:33 -0500

    feat(generator): add map information to type information

--- world/generator/parsing/ast.go
+++ world/generator/parsing/ast.go
@@ -13,8 +13,14 @@ type GoStruct struct {
 type GoType struct {
 	Value    string
 	IsArray  bool
+	IsMap    bool
 	Nillable bool
+
+	MapKey *GoType
+	MapValue *GoType
 }
+
+
 
 func FindStructures(fileAST *ast.File) []*GoStruct {
 	structures := make([]*GoStruct, 0)
@@ -47,13 +53,19 @@ func FindStructures(fileAST *ast.File) []*GoStruct {
 func GoTypeFromExpr(e ast.Expr) GoType {
 	switch v := e.(type) {
 	case *ast.Ident:
-		return GoType{v.Name, false, false}
+		return GoType{v.Name, false, false, false, nil, nil}
 	case *ast.ArrayType:
-		return GoType{"[]" + GoTypeFromExpr(v.Elt).Value, true, true}
+		return GoType{"[]" + GoTypeFromExpr(v.Elt).Value, true, false, true, nil, nil}
 	case *ast.StarExpr:
-		return GoType{"*" + GoTypeFromExpr(v.X).Value, false, true}
+		return GoType{"*" + GoTypeFromExpr(v.X).Value, false, false, true, nil, nil}
 	case *ast.SelectorExpr:
-		return GoType{GoTypeFromExpr(v.X).Value + "." + v.Sel.Name, false, false}
+		return GoType{GoTypeFromExpr(v.X).Value + "." + v.Sel.Name, false, false ,false, nil, nil}
+	case *ast.MapType:
+		keyType := GoTypeFromExpr(v.Key)
+		valueType := GoTypeFromExpr(v.Value)
+		return GoType{"map[" + keyType.Value + "]" + valueType.Value, false, true, true, &keyType, &valueType}
+	case *ast.InterfaceType:
+		return GoType{"interface{}", false, false, true, nil, nil}
 	default:
 		panic(errors.New("unknown type"))
 	}
