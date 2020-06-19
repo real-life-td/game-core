d72f9b8951ad5e01173264a605c7bef1fa214f11
Joseph Naberhaus <naberhausj@gmail.com>
2020-06-18 21:19:09 -0500

    feat(generator): produce actions in correct order

--- world/generator/operations/operations.go
+++ world/generator/operations/operations.go
@@ -3,6 +3,7 @@ package operations
 import (
 	"github.com/dave/jennifer/jen"
 	"github.com/real-life-td/game-core/world/generator/parsing"
+	"sort"
 )
 
 func GenerateOperations(file *jen.File, structs []*parsing.GoStruct) {
@@ -11,8 +12,13 @@ func GenerateOperations(file *jen.File, structs []*parsing.GoStruct) {
 		if len(stageOperations) != 0 {
 			writeOperationStructs(file, s.Name, stageOperations)
 
-			writeInitFunc(file, s.Name, stageOperations.get(initStage))
-			writeGameFunc(file, s.Name, stageOperations.get(gameStage))
+			writeInitFunc(file, s.Name, sortByPrecedence(stageOperations.get(initStage)))
+			writeGameFunc(file, s.Name, sortByPrecedence(stageOperations.get(gameStage)))
 		}
 	}
+}
+
+func sortByPrecedence(operations []*operation) []*operation {
+	sort.Slice(operations, func(i, j int) bool { return operations[i].action < operations[j].action })
+	return operations
 }
